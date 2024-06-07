e1629af10f924837408cbe4f300c9149127dbe30
AmanShahJashDS <aman.shah@jashds.com>
2024-06-08 00:28:28 +0530

    adding backend apis

---
+++ backend/backend_api.py
@@ -0,0 +1,224 @@
+from fastapi import FastAPI,Query, Depends, File, UploadFile
+from datetime import datetime 
+from sqlalchemy.orm import  Session
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+UPLOAD_DIRECTORY = "/home/ec2-user/resumes"  
+
+DB_HOST = 'screeners.c3iyi0cuiwty.us-east-1.rds.amazonaws.com'
+DB_PORT = '5432'
+DB_NAME = 'postgres'
+DB_USER = 'postgres'
+DB_PASSWORD = 'jashds48'
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+        print("db connected")
+    except Exception as e:
+        raise
+    finally:
+        db.close()
+        
+def shutdown_db_client():
+    SessionLocal.close_all()
+
+
+@app.post("/signup")
+async def signup(
+    user_role_id: str = Query(...),
+    org_name:str = Query(...),
+    org_desc:str = Query(...),
+    user: str = Query(...),
+    password: str = Query(...),
+    db: Session = Depends(get_db)
+):
+
+    try:
+        existing_user_query = text("SELECT * FROM screener.users WHERE username=:user")
+        existing_user = db.execute(existing_user_query, {"user": user}).fetchall()
+
+        if existing_user:
+            return {"status_code": 409, "detail": "User already exists"}
+
+        insert_org_query = text("INSERT INTO screener.organizations (name, org_desc) VALUES (:org_name, :org_desc)")
+        db.execute(insert_org_query, {"org_name": org_name, "org_desc": org_desc})
+        org_id_query = text("SELECT org_id FROM screener.organizations WHERE name=:org_name")
+        org_id=db.execute(org_id_query, {"org_name": org_name}).fetchone()
+        print(org_id)
+
+
+        insert_user_query = text("INSERT INTO screener.users (username, password , user_role ,org_id) VALUES (:user, :hashed_password, :user_role, :org_id)")
+        db.execute(insert_user_query, {"user": user, "hashed_password":password, "user_role": user_role_id.lower() ,"org_id":org_id[0]})
+        db.commit()
+
+        return {
+            "Success": True,
+            "message": "User signed up successfully"
+        }
+    except Exception as e:
+        error_message = str(e)
+        print("Error:", error_message)
+        error_response = {
+            "Success": False,
+            "Timestamp": datetime.now(),
+            "Error": error_message
+        }
+        return {"status_code": 400, "detail": error_response}
+
+
+
+
+
+
+@app.get("/login")
+async def login(
+    user: str = Query(...),
+    password: str = Query(...),
+    db: Session = Depends(get_db)
+):
+    if not user or not password:
+        return {"status_code": 409, "detail": "Incorrect email or password"}
+
+    try:
+        print(f"user : {user}")
+        password_query = text("SELECT password FROM screener.users WHERE username='{}'".format(user))
+        print(password_query)
+        stored_password = db.execute(password_query,{"user": user}).fetchone()[0]
+        
+        print(f"password : {stored_password}")
+
+        
+        user_role_query=text("SELECT user_role FROM screener.users WHERE username ='{}'".format(user))
+        user_role = db.execute(user_role_query,{"user": user}).fetchone()[0]
+        print(f"user_role : {user_role}")
+
+        if not stored_password:
+            return {"status_code": 400, "detail": "Incorrect email or password"}
+        else:
+            print("password matched")
+        
+        if user_role=='candidate':
+            organizations_query=text("SELECT name,org_id,org_desc FROM screener.organizations")
+            organizations = db.execute(organizations_query).fetchall()
+            print(organizations)
+            organizations_data = [
+                    {
+                        "org_name": result.name,
+                        "org_id": result.org_id,
+                        "org_desc": result.org_desc
+                    }
+                    for result in organizations
+                    if result.name != ""
+                ]
+
+            
+            response_data = {
+                "organizations_data":organizations_data,
+                "username":user,
+                "user_role":user_role,
+                "Success": True,
+                "timestamp": str(datetime.now()),
+            }
+            
+            # elif user_role=='recruiter':
+            
+            return response_data
+        else:
+            return {"status_code": 400, "detail": "Incorrect email or password"}
+
+    except Exception as e:
+        error_message = str(e)
+        print("Error:", error_message)
+        error_response = {
+            "Success": False,
+            "Timestamp": str(datetime.now()),
+            "Error": error_message
+        }
+        return {"status_code": f"exception raised : {e}"}
+
+
+@app.get("/job_list")
+async def list_jobs(
+    org_id: str = Query(...),
+    db: Session = Depends(get_db)
+):  
+
+    try :
+        jobs_query=text("SELECT job_role,job_id,job_status FROM screener.job_details WHERE org_id ='{}'".format(org_id))
+        jobs = db.execute(jobs_query, {'org_id': org_id}).fetchall()
+        print(f"jobs : {jobs}")
+        jobs_data = []
+        for result in jobs:
+            print(f"result : {result}")
+            try :
+                job_req_query=text("SELECT primary_skills,secondary_skills,required_exp FROM screener.job_requirements where job_id='{}'".format(result.job_id))
+                req_query = db.execute(job_req_query, {'job_id': result.job_id}).fetchone()
+
+                print(f"req_query : {req_query}")
+
+                job_data = {
+                    "job_role": result.job_role,
+                    "job_id": result.job_id,
+                    "job_status": result.job_status,
+                    "job_primary_skills": ','.join(req_query.primary_skills),
+                    "job_secondary_skills": ','.join(req_query.secondary_skills),
+                    "required_exp": req_query.required_exp
+                }
+                jobs_data.append(job_data)
+            except Exception as e:
+                print(f"error : {e}")
+    
+        response_data = {
+                    "jobs_data":jobs_data,
+                    "Success": True,
+                    "timestamp": str(datetime.now()),
+                }
+        return response_data
+    
+    except Exception as e:
+
+        error_message = str(e)
+        print("Error:", error_message)
+        error_response = {
+            "Success": False,
+            "Timestamp": str(datetime.now()),
+            "Error": error_message
+        }
+        return {"status_code": f"exception raised : {e}"}
+
+@app.post("/analysis")
+async def upload_pdf(pdf_file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIRECTORY, pdf_file.filename)
+    with open(file_path, "wb") as file_object:
+        file_object.write(await pdf_file.read())
+        
+    
---
+++ backend/engine.py
@@ -0,0 +1,156 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv, find_dotenv
+import os
+import PyPDF2
+
+# load API keys
+_ = load_dotenv(find_dotenv())
+
+class GenEngine():
+    def __init__(self):
+        MODEL_NAME = "claude-3-opus-20240229"
+        self.llm = ChatAnthropic(
+                    model=MODEL_NAME,
+                    temperature=0,
+                    max_tokens=1024,
+                    timeout=None,
+                    max_retries=2,
+                    # other params...
+                )
+        self.messages = [
+                            SystemMessage(
+                                content="""
+                        You are AI interviewer who need to generate questions given job description provided by recruiter and candidate's skill extracted from the resume.
+
+                        Instruction to generate questions:
+                        1. Questions should cover all the primary skills, some of the secondary skills and candidate's skills
+                        2. Generate 20 question
+                        3. Questions should be around fundamentals involved in the skills mentioned
+                        4. Along with questions provide the skills on which you are evaluating. Note: skills should be from the given context only.
+
+                        # Ouptut Format:
+                        {"questions": [q1, q2, ...]}
+                        """
+                            )
+                            ]
+
+    def invoke(self, skills, cursor, job_id):
+        job_text = cursor.execute("query")
+        self.messages.append(HumanMessage(
+                                    content=f"""
+                                    ### Job Description:
+                                    {job_text}
+
+                                    ### Candidate's Skill:
+                                    {skills}
+                                    """
+                                    ))
+        return self.llm.invoke(self.messages)
+    
+class ParseEngine():
+    def __init__(self):
+        MODEL_NAME = "claude-3-opus-20240229"
+        self.llm = ChatAnthropic(
+                    model=MODEL_NAME,
+                    temperature=0,
+                    max_tokens=1024,
+                    timeout=None,
+                    max_retries=2,
+                    # other params...
+                )
+
+        self.messages = [
+                    SystemMessage(
+                                content="""
+                        You are provided with Resume of the candidate and Job description.
+                        Job description has keys: Designation, Experience, Primary Skills, Secondary Skills
+
+                        Extract following keywords from the resume which are matching with the Job Description:
+                        1. Years of Experience
+                        2. Key Skills
+
+                        It would be ideal if we have one skill from resume that is relevant to a different skill in job description.
+
+                        Output Format:
+                        ### JSON
+                        ```
+                        {
+                        "experience": # years of experience if provided, fresher if mentioned or Unknown of not mentioned 
+                        "key_skills": # list of skills that are matching with job description, order by most relevant skills along with skills mentioned in job description
+                        }
+                        ```
+                        """
+                    ),
+                    ]
+
+    def invoke(self, pdf_path, cursor, job_id):
+        resume_text = self.parse_resume(pdf_path)
+        job_text = cursor.execute("query")
+        self.messages.append(HumanMessage(
+                                    content=f"""
+                                    ### Resume:
+                                    {resume_text}
+
+                                    ### Job Description:
+                                    {job_text}
+                                    """
+                                    ))
+        return self.llm.invoke(self.messages)
+
+    def parse_resume(self, pdf_path):
+        # Open the PDF file
+        with open(pdf_path, 'rb') as file:
+            # Create a PDF reader object
+            pdf_reader = PyPDF2.PdfReader(file)
+
+            # Get the number of pages in the PDF
+            num_pages = len(pdf_reader.pages)
+
+            # Initialize a variable to store the extracted text
+            text = ""
+
+            # Loop through each page and extract text
+            for page_num in range(num_pages):
+                page = pdf_reader.pages[page_num]
+                text += page.extract_text()
+
+        return text
+
+
+class EvalEngine():
+    def __init__(self):
+        MODEL_NAME = "claude-3-opus-20240229"
+        self.llm = ChatAnthropic(
+                    model=MODEL_NAME,
+                    temperature=0,
+                    max_tokens=1024,
+                    timeout=None,
+                    max_retries=2,
+                    # other params...
+                )
+
+        self.messages = [
+                    SystemMessage(
+                                content="""
+                        Given question answer pair you need to evaluate candidate's answer on each question.
+                        Steps to evaluate:
+                        1. Extract points to be covered to answer the given question
+                        2. Evaluate if most of the points are covered for given answer
+                        3. Rate the answer based on the points covered in the range of 1-10
+                        4. Don't give higher rating if there are only key words without much explanation
+
+                        Output Format:
+                        ### JSON
+                        {"q1": #rating (int), "q2": #rating (int), # similarly for other questions}
+                        """
+                    ),
+                    ]
+
+    def invoke(self, qa_pairs):
+        self.messages.append(HumanMessage(
+                                    content=f"""
+                                    {qa_pairs}
+                                    """
+                                    ))
+        return self.llm.invoke(self.messages)
